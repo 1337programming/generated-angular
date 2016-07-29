269ef528f3185fd98bc0a972893c3d7f3757ab3d
Tom Manion <tom.manion@autodesk.com>
2016-07-29 14:02:22 -0400

    Generated Application

---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name generatedAngularApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the generatedAngularApp
+ */
+angular.module('generatedAngularApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
